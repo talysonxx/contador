0fb650e08104c2e2b5c65bdb1db8c48af1cfb0e6
talysonxx <talysonoficial12@gmail.com>
2021-08-30 17:17:46 -0300

    relogio feito

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import React, {useState} from 'react'
 
 export default function App() {
+  setInterval(getTime, 1000)
+  
+  const [time, setTime] = useState()
+
+  function getTime(){
+    const date = new Date()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+    const now = `${hour}:${minute}:${second}`
+    setTime(now)
+    document.title = 'Time: ' + now
+  }
+
   return (
-    <div>
-      sim
+    <div className='timer-container'>
+      <span className='timer'>{time}</span>
     </div>
   );
 
